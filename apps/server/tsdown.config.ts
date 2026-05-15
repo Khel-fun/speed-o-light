5f3b107329b35789f6e146929a74a1c81ff4b358
/ nandagopan . <163506149+0xnandagopan@users.noreply.github.com>
2026-05-16 01:56:08 +0530

    [ FIX] deployment - dirname issuue

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -3,6 +3,18 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: "./src/index.ts",
   format: "esm",
+  external: [
+      /^@noir-lang\//,
+      /^@aztec\//,
+    ],
+    banner: {
+           js: `
+                  import { fileURLToPath as __fileURLToPath } from 'url';
+                  import { dirname as __dirname_fn } from 'path';
+                  const __filename = __fileURLToPath(import.meta.url);
+                  const __dirname = __dirname_fn(__filename);
+               `
+            },
   outDir: "./dist",
   clean: true,
   noExternal: [/@speed-o-light\/.*/],
