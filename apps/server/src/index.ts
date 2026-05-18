1d80ad79315e618bef13ae0c3faa248a4a100694
ayushk4543 <sanusingh335@gmail.com>
2026-05-18 14:18:09 +0530

    add credentials in header

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -19,7 +19,8 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "ngrok-skip-browser-warning"],
+    allowedHeaders: ["Content-Type", "ngrok-skip-browser-warning", "x-trpc-source"],
+    credentials: true,
   }),
 );
 
